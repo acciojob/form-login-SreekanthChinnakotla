9580373d0e886ab00c3a47d76f95f6f21f5e215e
SreekanthChinnakotla <134213780+SreekanthChinnakotla@users.noreply.github.com>
2023-10-04 14:52:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	let firstname = document.getElementsByName("fname")[0].value;
+	let lastname =  document.getElementsByName("lname")[0].value;
+	const message = firstname +" "+lastname;
+	alert(message);
 
 }
